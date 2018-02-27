66d0c4818c552aeb8b1090472c0446cf475f301a
unknown <dudung@gmail.com>
2018-02-27 13:31:05 +0700

    Rename visual/point2.js to visual/electronics/point.js

---
+++ visual/electronics/point.js
@@ -0,0 +1,71 @@
+/*
+	point.js
+	Point in 2-d for drawing, especially in electronics
+	circuit
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180107
+	Create this object.
+	20180227
+	Modify this library and rename it to Point.
+*/
+
+// Define class of Point
+class Point {
+	// Create constructor
+	constructor() {
+		this.x = 0;
+		this.y = 0;
+		this.color = int2rgb(0, 0, 0);
+		this.size = 2;
+		this.r = new Vect3();
+		
+		if(arguments.length == 2) {
+			this.x = arguments[0];
+			this.y = arguments[1];
+		}
+		
+		if(arguments.length == 1) {
+			this.r = arguments[0];
+			this.setXY();
+		}
+	}
+	
+	// Convert from Vect3
+	setXY() {
+		this.x = this.r.x;
+		this.y = this.r.y;
+	}
+	setYZ() {
+		this.x = this.r.y;
+		this.y = this.r.z;
+	}
+	setZX() {
+		this.x = this.r.z;
+		this.y = this.r.x;
+	}
+	setYX() {
+		this.x = this.r.y;
+		this.y = this.r.x;
+	}
+	setZY() {
+		this.x = this.r.z;
+		this.y = this.r.y;
+	}
+	setXZ() {
+		this.x = this.r.x;
+		this.y = this.r.z;
+	}
+	
+	// Get string value
+	strval() {
+		var s = "(";
+		s += this.x + ", ";
+		s += this.y + ", ";
+		s += this.color + ", ";
+		s += this.size + ", ";
+		s += ")";
+		return s;
+	}
+}
--- visual/painter.js
+++ visual/painter.js
@@ -13,6 +13,7 @@ class Painter {
 	// Create constructor
 	constructor() {
 		this.id = arguments[0];
+		this.coords = arguments[1];
 	}
 	
 	setCanvasId(id) {
@@ -21,12 +22,23 @@ class Painter {
 	
 	draw() {
 		var can = document.getElementById(this.id);
+		var cx = can.getContext("2d");
 		
 		var N = arguments.length;
 		var objs = arguments;
 		
 		for(var i = 0; i < N; i++) {
-						
+			var o = objs[i];
+			if(o instanceof Point2) {
+				cx.beginPath();
+				cx.strokeStyle = o.c;
+				var q = coords.transform({x: o.x, y: o.y});
+				console.log(q.x + " " + q.y + " " + o.s);
+				cx.arc(q.x, q.y, o.s, 0, 2 * Math.PI);
+				cx.fillStyle = o.c;
+				cx.fill();
+				cx.stroke();
+			}
 		}
 	}
 }
