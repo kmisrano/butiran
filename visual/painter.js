573db9d10602563810b8b2d9100b1632d603aded
unknown <dudung@gmail.com>
2018-02-28 10:27:39 +0700

    Fix visual of Capacitor in visual/painter.js

--- visual/painter.js
+++ visual/painter.js
@@ -6,6 +6,9 @@
 	
 	20180227
 	Create this library.
+	20180228
+	Modify this library but still false ~ 0533.
+	Fix the bug at 1026.
 */
 
 // Define class of Painter
@@ -56,43 +59,48 @@ class Painter {
 				cx.strokeStyle = o.color;
 				cx.fillStyle = o.color;
 				
-				var l = 2 * o.plateThickness + o.plateGap;
+				var w = 2 * o.plateThickness + o.plateGap;
 				var qbeg = coords.transform(o.rbeg);
 				var qend = coords.transform(o.rend);
 				var Dx = qend.x - qbeg.x;
 				var Dy = qend.y - qbeg.y;
 				var L = Math.sqrt(Dx * Dx + Dy * Dy);
 				
-				var c = 0.5 * (L - l) / L;
+				var cosq = Dx / L;
+				var sinq = Dy / L;
+				console.log(cosq + " " + sinq);
+				var l = 0.5 * (L - w);
 				
 				var q1 = {};
-				q1.x = qbeg.x + Dx * c;
-				q1.y = qbeg.y + Dy * c;
+				q1.x = qbeg.x + l * cosq;
+				q1.y = qbeg.y + l * sinq;
 				var q2 = {};
-				q2.x = qend.x - Dx * c;
-				q2.y = qend.y - Dy * c;
+				q2.x = qend.x - l * cosq;
+				q2.y = qend.y - l * sinq;
 				
 				cx.moveTo(qbeg.x, qbeg.y);
 				cx.lineTo(q1.x, q1.y);
 				cx.moveTo(qend.x, qend.y);
 				cx.lineTo(q2.x, q2.y);
 				
+				// 
+				
 				//-------------------------------------
 				// This part is still false
 				//-------------------------------------
 				
 				var q1u = {};
-				q1u.x = q1.x;
-				q1u.y = q1.y - 0.5 * o.size;
+				q1u.x = q1.x - 0.5 * o.size * sinq;
+				q1u.y = q1.y + 0.5 * o.size * cosq;
 				var q1d = {};
-				q1d.x = q1.x;
-				q1d.y = q1.y + 0.5 * o.size;
+				q1d.x = q1.x + 0.5 * o.size * sinq;
+				q1d.y = q1.y - 0.5 * o.size * cosq
 				var q11u = {};
-				q11u.x = q1u.x + o.plateThickness;
-				q11u.y = q1u.y;
+				q11u.x = q1u.x + o.plateThickness * cosq;
+				q11u.y = q1u.y + o.plateThickness * sinq;
 				var q11d = {};
-				q11d.x = q1d.x + o.plateThickness;
-				q11d.y = q1d.y;
+				q11d.x = q1d.x + o.plateThickness * cosq;
+				q11d.y = q1d.y + o.plateThickness * sinq;
 				cx.moveTo(q1u.x, q1u.y);
 				cx.lineTo(q11u.x, q11u.y);
 				cx.lineTo(q11d.x, q11d.y);
@@ -100,17 +108,17 @@ class Painter {
 				cx.closePath();
 				
 				var q2u = {};
-				q2u.x = q2.x;
-				q2u.y = q2.y - 0.5 * o.size;
+				q2u.x = q2.x - 0.5 * o.size * sinq;
+				q2u.y = q2.y + 0.5 * o.size * cosq
 				var q2d = {};
-				q2d.x = q2.x;
-				q2d.y = q2.y + 0.5 * o.size;
+				q2d.x = q2.x + 0.5 * o.size * sinq;
+				q2d.y = q2.y - 0.5 * o.size * cosq;
 				var q22u = {};
-				q22u.x = q2u.x - o.plateThickness;
-				q22u.y = q2u.y;
+				q22u.x = q2u.x - o.plateThickness * cosq;
+				q22u.y = q2u.y - o.plateThickness * sinq;
 				var q22d = {};
-				q22d.x = q2d.x - o.plateThickness;
-				q22d.y = q2d.y;
+				q22d.x = q2d.x - o.plateThickness * cosq;
+				q22d.y = q2d.y - o.plateThickness * sinq
 				cx.moveTo(q2u.x, q2u.y);
 				cx.lineTo(q22u.x, q22u.y);
 				cx.lineTo(q22d.x, q22d.y);
