be17b4519160c8a3f7667a2f00278afd698819fc
unknown <dudung@gmail.com>
2018-03-02 15:31:28 +0700

    Test Timer

---
+++ z/test_timer.js
@@ -0,0 +1,26 @@
+/*
+	test_timer.js
+	Test the use of Timer
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180302
+	Create this test function.
+*/
+
+// 20180302.1520 ok
+function test_timer() {
+	var timer = new Timer(run, 100);
+	
+	var N = 10;
+	var i = 0;
+	timer.start();
+	
+	function run() {
+		console.log(i);
+		if(i >= N) {
+			timer.stop();
+		}
+		i++;
+	}
+}
