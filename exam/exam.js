e427c0d90ca35f659711e514cb84c8cd0d88b9ba
dudung <dudung@gmai.com>
2018-03-03 23:48:39 +0700

    Start exam and its problem

---
+++ exam/exam.js
@@ -0,0 +1,63 @@
+/*
+	exam.js
+	Represent problems for exam in JS
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180303
+	Start this library.
+*/
+
+// 20180303.2347 ok
+function selectProblem() {
+	var target = window.event.target;
+	var value = target.value;
+	switch(value) {
+		case "Select problems":
+			examClear();
+			break;
+		case "Hello world":
+			examHelloWorld();
+			break;
+		case "Display series":
+			examDisplaySeries();
+			break;
+		case "Root formula":
+			examMathJaxRootFormula();
+			break;
+		default:
+	}
+}
+
+// 20180303.2347 ok
+function examMathJaxRootFormula() {
+	var div = document.getElementById("scriptResult");	
+	var str = "";
+	str += "\\begin{equation}";
+	str += "x_{1,2} = \\frac{-b \\pm \\sqrt{b^2 - 4ac}}{2a}";
+	str += "\\end{equation}";
+	updateMath("scriptResult", str);
+}
+
+// 20180303.2308 ok
+function examDisplaySeries() {
+	var div = document.getElementById("scriptResult");
+	var N = 10;
+	var str = "";
+	for(var i = 0; i < N; i++) {
+		str += i + "<br/>";
+	}
+	div.innerHTML = str;
+}
+
+// 20180303.2249 ok
+function examClear() {
+	var div = document.getElementById("scriptResult");
+	div.innerHTML = "&nbsp;";	
+}
+
+// 20180303.2249 ok
+function examHelloWorld() {
+	var div = document.getElementById("scriptResult");
+	div.innerHTML = "Hello, World!";
+}
---
+++ z/test_exam.js
@@ -0,0 +1,45 @@
+/*
+	test_exam.js
+	Test the use of exam.js and related function
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180303
+	Start this test functions.
+*/
+
+function test_exam() {
+	var sel = document.createElement("select");
+	sel.addEventListener("change", selectProblem)
+	sel.style.fontFamily = "Arial";
+	sel.style.fontSize = "13px";
+	document.body.appendChild(sel);
+	
+	var div = document.createElement("div");
+	div.id = "scriptResult";
+	div.innerHTML = "&nbsp;"
+	div.style.border = "1px solid #888";
+	div.style.background = "#f8f8f8";
+	div.style.padding = "10px";
+	div.style.fontFamily = "Arial";
+	div.style.fontSize = "12px";
+	document.body.appendChild(div);
+	
+	var opt;
+	
+	opt = document.createElement("option");
+	opt.text = "Select problems";
+	sel.options.add(opt);
+	
+	opt = document.createElement("option");
+	opt.text = "Hello world";
+	sel.options.add(opt);
+	
+	opt = document.createElement("option");
+	opt.text = "Display series";
+	sel.options.add(opt);
+	
+	opt = document.createElement("option");
+	opt.text = "Root formula";
+	sel.options.add(opt);
+}
