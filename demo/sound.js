6ebbba7c0786d140b09fb2703c88204c6c0f9dd0
dudung <dudung@gmai.com>
2018-03-17 14:02:56 +0700

    Add simple instrument

---
+++ demo/sound.js
@@ -0,0 +1,72 @@
+/*
+	sound.js
+	Generate sound using pizzicato.js library
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180317
+	Start this demo file.
+*/
+
+// 20180317.1325 !ok
+function demoSimpleInstrument() {
+	var baseFrequency = 440;
+	var sineWave = new Pizzicato.Sound({ 
+    source: 'wave', 
+    options: {
+        frequency: baseFrequency
+    }
+	});
+	
+	var baseFrequency = 440;
+	var ratio = [1/1, 9/8, 5/4, 4/3, 3/2, 5/3, 15/8, 2/1];
+	var label = ["C", "D", "E", "F", "G", "A", "B", "C"];
+	for(var i = 0; i < ratio.length; i++) {
+		var b = document.createElement("button");
+		b.innerHTML = label[i];
+		b.id = i;
+		document.body.appendChild(b);
+		b.addEventListener("mousedown", playSound);
+		b.addEventListener("mouseup", stopSound);
+		b.addEventListener("mouseout", stopSound);
+	}
+	
+	//sineWave.play();
+	
+	function playSound() {
+		var t = event.target;
+		var f = baseFrequency * ratio[t.id];
+		sineWave.frequency = f;
+		sineWave.play();
+	}
+	
+	function stopSound() {
+		sineWave.stop();
+	}
+}
+
+// 20180317.1323 !ok 
+function demoToggleSound() {
+	var sineWave = new Pizzicato.Sound({ 
+    source: 'wave', 
+    options: {
+        frequency: 440
+    }
+	});
+	
+	var btn = document.createElement("button");
+	btn.innerHTML = "Play";
+	document.body.appendChild(btn);
+	btn.addEventListener("mousedown", btnClick);
+	
+	function btnClick() {
+		var t = event.target;
+		if(t.innerHTML == "Play") {
+			t.innerHTML = "Stop";
+			sineWave.play();
+		} else {
+			t.innerHTML = "Play";
+			sineWave.stop();
+		}
+	}
+}
