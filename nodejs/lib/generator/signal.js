9b6839d630ad7dd703319bf028145a55607c6138
dudung <dudung@gmai.com>
2018-05-19 12:29:53 +0700

    add nodejs lib/generator

---
+++ nodejs/lib/generator/signal.js
@@ -0,0 +1,16 @@
+/*
+	signal.js
+	An object for mimicking signal generator
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180519
+	Recreate this object by implementing node.js for butiran.
+*/
+
+module.exports = class SignalGenerator {
+	constructor() {
+		this.pos = 0;
+		this.coeff = []
+	}
+}
