940ca1f8a2a303803e3ea97ec72820666f0c9fab
dudung <dudung@gmai.com>
2018-05-20 00:02:30 +0700

    Compile butiran using webpack

---
+++ butiran/butiran.js
@@ -0,0 +1,19 @@
+/*
+	butiran.js
+	Simulation for granular particles system
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	Execute: node --experimental-modules butiran.mjs
+	
+	20180519
+	Start recreating this library, now with node.js support
+	and try to use ES modules, which is still experimental.
+*/
+
+var Sequence = require('./lib/sequence')();
+
+if(typeof window !== 'undefined') {
+	// Store to window object -- 20180519.2358
+	window["Sequence"] = Sequence;
+}
