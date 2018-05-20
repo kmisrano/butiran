88503e4f1dfa80e6d1ea46e71309f213a9777352
dudung <dudung@gmai.com>
2018-05-20 20:02:14 +0700

    create lib/resistor.js

---
+++ butiran/lib/resistor.js
@@ -0,0 +1,67 @@
+/*
+	resistor.js
+	An object for storing resistance value
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	20180520
+	Create this object as part of components in butiran.
+*/
+
+// Define class of Resistor
+class Resistor {
+	
+	// Define constructor
+	constructor() {
+		// Define default periodic values in one period
+		this.value = 0;
+		this.type = "";
+		
+		// Change default value if given as arguments
+		if(arguments.length == 1) {
+			this.type = "constant";
+			this.value = arguments[0];
+		} else if(arguments.length == 2) {
+			this.type = "variable";
+			this.Rmax = arguments[0];
+			this.pos = arguments[1];
+			this.value = this.Rmax * this.pos;
+		} else if(arguments.length == 3) {
+			this.type = "time-dependent";
+			this.Rmin = arguments[0];
+			this.Rmax = arguments[1];
+			this.tau = arguments[2];
+			this.value = this.Rmin;
+			this.V = 0;
+		}
+	}
+	
+	// Get value of resistor
+	ping() {
+		if(this.type == "constant") {
+			this.value = this.value;
+		} else if(this.type == "variable") {
+			this.pos = arguments[0];
+			this.value = this.Rmax * this.pos;
+		} else if(this.type == "time-dependent") {
+			var t = arguments[0];
+			var V = arguments[1];
+			var R = this.value;
+			var dV = (V - this.V);
+			var dR = 0;
+			if(dV > 0) {
+				dR = (this.Rmax - R) * t / tau;
+			} else {
+				dR = (this.Rmin - R) * t / tau;
+			}
+			R += dR;
+			this.value = R;
+		}
+		return this.value;
+	}
+}
+
+// Export module
+module.exports = function() {
+	return Resistor;
+};
