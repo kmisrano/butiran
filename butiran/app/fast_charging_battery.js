a00db0986319900a9dc4d665bcc2cb22c2e8379e
dudung <dudung@gmai.com>
2018-05-20 15:49:10 +0700

    capacitor_charging.html: app --> sandbox, fast_charging_battery.js:butiran/app

---
+++ butiran/app/fast_charging_battery.js
@@ -0,0 +1,25 @@
+/*
+	fast_charging_battery.js
+	Simulation for battery charging based on capacitor model
+	
+	Sparisoma Viridi | dudung@gmail.com
+	
+	Execute: node fast_charging_battery.js
+	
+	Version info:
+		Node.js	v10.1.0
+	
+	20180512
+	Create this application as empty HTML file.
+	20180513
+	Start to build the layout.
+	20180518
+	Continue developing the application.
+	20180520
+	Change filename from app/capacitor_charging.html to
+	app/butiran/app/fast_charging_battery.js as part of
+	butiran.js project, and move the old one into sandbox.
+*/
+
+// Require classes
+var Sequence = require('../lib/sequence')();
