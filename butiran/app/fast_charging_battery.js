1284535f5140c7f2c4a738af84f41a5596a1a595
dudung <dudung@gmai.com>
2018-05-20 16:52:59 +0700

    create generator.js and move the old to sandbox

--- butiran/app/fast_charging_battery.js
+++ butiran/app/fast_charging_battery.js
@@ -4,7 +4,7 @@
 	
 	Sparisoma Viridi | dudung@gmail.com
 	
-	Execute: node fast_charging_battery.js
+	Execute: node app/fast_charging_battery.js
 	
 	Version info:
 		Node.js	v10.1.0
@@ -23,3 +23,41 @@
 
 // Require classes
 var Sequence = require('../lib/sequence')();
+var Generator = require('../lib/generator')();
+
+// Execute main function in development stage
+main();
+
+// Define main function
+function main() {
+	
+	// Define time step
+	var dt = 0.001; // s
+
+	// Define pattern for sequence as voltage source
+	var ptn1 = [
+		0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+		1, 1, 1, 1, 1, 1, 1, 1, 1, 1
+	];
+	var ptn2 = [
+		0, 0,
+		1, 1
+	];
+	
+	// Define sequences
+	var seq1 = new Sequence(ptn1);
+	var seq2 = new Sequence(ptn2);
+
+	// Define signal amplitudo
+	var amp1 = 10; // V
+	var amp2 = 15; // V
+	
+	// Define generator
+	var gen = new Generator(dt, [seq1, seq2], [amp1, amp2]);
+	
+	var N = 25;
+	for(var i = 0; i < N; i++) {
+		var signal = gen.ping();
+		console.log(signal);
+	}
+}
