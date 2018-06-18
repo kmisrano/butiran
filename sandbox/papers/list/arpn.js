77dcb28df9ff6da2cc602d82972185a711ba9f26
dudung <dudung@gmail.com>
2018-06-19 02:56:41 +0700

    Archive sandbox/papers

---
+++ sandbox/papers/list/arpn.js
@@ -0,0 +1,27 @@
+// ------------------------------------
+// ARPN Journal
+// ------------------------------------
+
+var p = new Journal(
+	"Suprijadi, T. Muliawan, S. Viridi",
+	"Automatic Road Lighting System (ARLS) Based on Images from Digital Camera",
+	"ARPN Journal of Science and Technology",
+	"3",
+	"12",
+	"1105-1109",
+	"2013",
+	"http://www.ejournalofscience.org/Download_Dec13_pdf_1.php"
+);
+pubs.push(p);
+
+var p = new Journal(
+	"C. F. Naa, Suprijadi, S. Viridi, M. Djamal",
+	"Novel Giant Magnetoresistance Model using Multiple Barrier Potential",
+	"ARPN Journal of Engineering and Applied Sciences",
+	"9",
+	"2",
+	"135-139",
+	"2014",
+	"http://www.arpnjournals.com/jeas/volume_02_2014.htm"
+);
+pubs.push(p);
---
+++ sandbox/papers/list/iopcs.js
@@ -0,0 +1,63 @@
+// ------------------------------------
+// AIP Conference Proceedings
+// ------------------------------------
+
+var p = new IOPConfSeries(
+	"D. Aprianti, F. Haryanto, A. Purqon, S. N. Khotimah, S. Viridi",
+	"Study of Budding Yeast Colony Formation and its Characterizations by using Circular Granular Cell",
+	"Journal of Physics: Conference Series",
+	"694",
+	"1",
+	"012079",
+	"2016",
+	"http://dx.doi.org/10.1088/1742-6596/694/1/012079"
+);
+pubs.push(p);
+
+var p = new IOPConfSeries(
+	"A. Sutresno, F. Haryanto, S. Viridi, I. Arif",
+	"Investigation of Diffusion Process inside the Cell using Monte Carlo Cell (MCell)",
+	"Journal of Physics: Conference Series",
+	"694",
+	"1",
+	"012077",
+	"2016",
+	"http://dx.doi.org/10.1088/1742-6596/694/1/012077"
+);
+pubs.push(p);
+
+var p = new IOPConfSeries(
+	"T. Kesuma, D. P. P. Aji, S. Viridi, Suprijadi",
+	"Influence of Contactopy on Two-Dimensional Brazil-Nut Effect",
+	"IOP Conference Series: Earth and Environmental Science",
+	"31",
+	"1",
+	"012001",
+	"2016",
+	"http://dx.doi.org/10.1088/1755-1315/31/1/012001"
+);
+pubs.push(p);
+
+var p = new IOPConfSeries(
+	"W. Yusman, S. Viridi, S. Rachmat",
+	"Geothermal Well Behaviour Prediction after Air Compress Stimulation using One-Dimensional Transient Numerical Modelling",
+	"IOP Conference Series: Earth and Environmental Science",
+	"29",
+	"1",
+	"012008",
+	"2016",
+	"http://dx.doi.org/10.1088/1755-1315/29/1/012008"
+);
+pubs.push(p);
+
+var p = new IOPConfSeries(
+	"A. Pattisahusiwa, A. Purqon, S. Viridi",
+	"Hydrostatic Simulation of Earth's Atmospheric Gas Using Multi-particle Collision Dynamics",
+	"IOP Conference Series: Earth and Environmental Science",
+	"29",
+	"1",
+	"012006",
+	"2016",
+	"http://dx.doi.org/10.1088/1755-1315/29/1/012006"
+);
+pubs.push(p);
