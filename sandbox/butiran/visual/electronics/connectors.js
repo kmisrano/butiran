0ad44a0be76608941a344113b2b9afa439376bd2
dudung <dudung@gmail.com>
2018-09-05 18:44:40 +0700

    Erase all

--- sandbox/butiran/visual/electronics/connectors.js
+++
@@ -1,1 +0,0 @@
-/*	connectors.js	Draw connectors in canvas element		Sparisoma Viridi | dudung@gmai.com		20180227	Create this library insipred by capacitors.js	in the same folder.*/class Connector {	constructor(r1, r2) {		this.type = "straight";		this.rbeg = r1;		this.rend = r2;		this.color = "#000";	}		setColor(lc) {		this.color = lc;	}}
--- sandbox/die/t_iteration_for.js
+++
@@ -1,9 +0,0 @@
-
-function iteration(N) {
-	for(var i = 0; i < N; i++) {
-		console.log(i);
-	}	
-}
-
-iteration(2);
-iteration(5);
