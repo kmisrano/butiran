70de8f7458ce75406d12d54afaec9b5770777045
dudung <dudung@gmail.com>
2018-09-06 04:41:42 +0700

    m butiran, r math_matrix, a math_polynomial

--- app/butiran.js
+++ app/butiran.js
@@ -10,9 +10,32 @@
 	20180905
 	Recreate this library and merge all into this single file
 	in order to simplify maintenance.
+	20180906
+	Confuse in naming convention of variables, functions, and
+	filenames.
 */
 
-// Matrix
-function btZeroMatrix(ROW, COL) {
+/*
+	Polynomial
 	
-}
+	20180906
+	Redefine class of Polynomial to get better consistency in
+	writing the code.
+*/
+class Polynomial {
+	// Constructor
+	constructor() {
+		this.coefs = [];
+		this.length = arguments.length;
+		
+		var N = this.length;
+		var coefs = [];
+		if(N > 0) {
+			for(var i = 0; i < N; i++) {
+				coefs.push(arguments[i]);
+			}
+		}
+		this.coefs = coefs;
+	}
+};
+
