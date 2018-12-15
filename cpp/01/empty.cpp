b6f9c89d9c5a921203054a12c923165ee874d788
dudung <dudung@gmail.com>
2018-12-16 05:43:15 +0700

    add 01/empty

---
+++ cpp/01/empty.cpp
@@ -0,0 +1,23 @@
+/*
+	empty.cpp
+	Template for code
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ empty.cpp -o empty
+	Execute: ./empty
+	
+	20181216
+	Start this template derived from 02/hello.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	// Comment for something
+	
+	return 0;
+}
