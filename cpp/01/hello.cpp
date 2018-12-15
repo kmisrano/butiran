ce1df737ec6b18db581b03d986fb9ff1d8c5a931
dudung <dudung@gmail.com>
2018-12-15 19:53:14 +0700

    create cpp/01

---
+++ cpp/01/hello.cpp
@@ -0,0 +1,23 @@
+/*
+	hello.cpp
+	Print "Hello world!" to console
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ hello.cpp -o hello
+	Execute: ./hello
+	
+	20181215
+	Start this program.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	cout << "Hello, world!" << endl;
+	
+	return 0;
+}
