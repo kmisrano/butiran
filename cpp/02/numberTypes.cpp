830aa2ca76fcbd8a2afdf8dfe94f62cff69760d6
dudung <dudung@gmail.com>
2018-12-16 16:47:19 +0700

    add 02/numberTypes

---
+++ cpp/02/numberTypes.cpp
@@ -0,0 +1,80 @@
+/*
+	numberTypes.cpp
+	Determine type of numbers
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ numberTypes.cpp -o numberTypes
+	Execute: ./numberTypes
+	
+	20181216
+	Start this program.
+*/
+
+// Include libraries
+#include <iostream>
+#include <cmath>
+#include <cstring>
+
+// Use namespace
+using namespace std;
+
+// Define main function
+int main(int argc, char *argv[]) {
+	// Check program argument
+	if(argc < 0) {
+		// Display program usage
+		cout << "Usage: numberTypes [number]" << endl;
+		
+		// Return unsuccess state
+		return -1;
+		
+	} else {
+		// Process number obtained from program argument
+		double x = atof(argv[1]);
+		
+		// Define type in string
+		string type = "";
+		
+		// Determine negative, positive, or zero
+		if(x < 0) {
+			type += "negative";
+			
+			// Determine integer or real
+			if(x == floor(x)) {
+				type += " integer";				
+			} else {
+				type += " real";			
+			}
+		} else if(x > 0) {
+			type += "positive";
+			
+			// Determine integer of real
+			if(x == floor(x)) {
+				type += " integer";
+				
+				// Determine whether square number
+				if(sqrt(x) == floor(sqrt(x))) {
+					type += " square";
+				}
+			} else {
+				type += " real";			
+			}
+		} else {
+			type += "zero";
+			
+			// Determine integer of real
+			if(x == floor(x)) {
+				type += " integer";
+			} else {
+				type += " real";			
+			}
+		}
+		
+		// Display result
+		cout << x << " is " << type << endl;
+		
+		// Return value, normally 0 means ok
+		return 0;
+	}	
+}
