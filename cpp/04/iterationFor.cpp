18b622617298623e4f66eacca57ee7f9db32e27c
unknown <dudung@gmail.com>
2018-12-20 19:16:41 +0700

    add 04/iteration-for

---
+++ cpp/04/iterationFor.cpp
@@ -0,0 +1,36 @@
+/*
+	iterationFor.cpp
+	Example of iteration with for
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ iterationFor.cpp -o iterationFor
+	Execute: ./iterationFor
+	
+	20181220
+	Start this program.
+*/
+
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int U(int);
+
+int main(int argc, char *argv[]) {
+	int N = 10;
+	
+	for(int n = 0; n <= N; n++) {
+		int Un = U(n);
+		cout << n << "\t";
+		cout << Un << endl;
+	}
+	
+	return 0;
+}
+
+int U(int n) {
+	int Un = 2 * n + 1;
+	return Un;
+}
