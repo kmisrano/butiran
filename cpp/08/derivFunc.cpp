62be26890880b26ad3a186c6b38df1375e507572
dudung <dudung@gmail.com>
2018-12-26 19:43:16 +0700

    add 08/deriv-func

--- cpp/08/derivFunc.cpp
+++ cpp/08/derivFunc.cpp
@@ -17,9 +17,19 @@
 #include <cstring>
 #include <vector>
 
+double f(double);
+
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	
 	return 0;
+}
+
+// Function f(x) = (x - x1) * (x - x2)
+double f(double x) {
+	double x1 = 3;
+	double x2 = 5;
+	double y = (x - x1) * (x - x2);
+	return y;
 }
