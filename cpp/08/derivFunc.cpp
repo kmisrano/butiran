59455e97ec088d0270b173a603e09ac9ce55787a
dudung <dudung@gmail.com>
2018-12-26 19:39:33 +0700

    add 08/deriv-func

---
+++ cpp/08/derivFunc.cpp
@@ -0,0 +1,25 @@
+/*
+	derivFunc.cpp
+	Derivative of a function
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ derivFunc.cpp -o derivFunc
+	Execute: ./derivFunc
+	
+	20181226
+	Start this program at Gambar station in Jakarta.
+*/
+
+#include <cmath>
+#include <iostream>
+#include <sstream>
+#include <cstring>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
