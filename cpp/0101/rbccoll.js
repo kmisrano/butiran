640e91ba90436403e085405889b457fed2ca663f
dudung <dudung@gmail.com>
2019-01-07 13:03:39 +0700

    cpp/0101/rbccoll

---
+++ cpp/0101/rbccoll.js
@@ -0,0 +1,27 @@
+/*
+	rbccoll.js
+	Collion of two RBCs
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Include: <script src="rbccoll.js"></script> in a HTML file
+	Execute: Refresh web browser viewing the HTML file
+	
+	20190107
+	Start this program at home in Bandung for today deadline of
+	manuscript for 18th AOCMP & 16th SEACOMP, 11-14 November
+	2018, Kuala Lumpur, Malaysia, url http://www.aocmp2018.com/.
+	Try to fuse md_rbcellips.js and md_fsgdods.js (20190101) files.
+	1254 Start physically by designing the interface.
+	
+*/	
+
+// Execute main function
+main();
+
+// Define main function
+function main() {
+	// show program name in console
+	var pname = "rbccoll.js";
+	console.log(pname);
+}
