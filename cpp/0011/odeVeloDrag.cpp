2dba6c4b31b4ea82d26a832eec93eb1397e816f2
unknown <dudung@gmail.com>
2019-01-12 14:22:38 +0700

    fin odeVeloDrag

--- cpp/0011/odeVeloDrag.cpp
+++ cpp/0011/odeVeloDrag.cpp
@@ -14,16 +14,18 @@
 
 #include <iostream>
 #include <cstdlib>
+#include <cmath>
 
 using namespace std;
 
 int main(int argc, char *argv[]) {
 	// Check arguments
-	if(argc < 4) {
-		cout << "Usage: odeVeloDrag [tbeg tend dt]" << endl;
+	if(argc < 5) {
+		cout << "Usage: odeVeloDrag [tbeg tend dt Tdata]" << endl;
 		cout << "tbeg\tinitial t" << endl;
 		cout << "tend\tfinal t" << endl;
 		cout << "dt\tstep of t" << endl;
+		cout << "Tdata\tperiod of output" << endl;
 		return 1;
 	}
 	
@@ -31,12 +33,36 @@ int main(int argc, char *argv[]) {
 	double tbeg = atof(argv[1]);
 	double tend = atof(argv[2]);
 	double dt = atof(argv[3]);
+	double Tdata = atof(argv[4]);
+	
+	// Set physical parameters
+	double v0 = 1;
+	double b = 1;
+	double m = 1;
+	
+	// Set output data
+	int Ndata = round(Tdata / dt);
+	int idata = Ndata;
 	
 	// Perform iteration
+	cout << "# t\tv_the\tv_num" << endl;
 	double t = tbeg;
+	double vthe = v0 * exp(-(b/m)*t);
+	double vnum = v0;
 	while(t <= tend) {
-		cout << t << endl;
+		
+		if(idata == Ndata) {
+			cout << t << "\t";
+			cout << vnum << "\t";
+			cout << vthe << endl;
+			idata = 0;
+		}
+		
+		vthe = v0 * exp(-(b/m)*t);
+		vnum *= (1 - (b/m) * dt);
+	
 		t += dt;
+		idata++;
 	}
 	
 	// Terminate program
