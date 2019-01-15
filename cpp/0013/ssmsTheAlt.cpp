1dc7fcf1ff7138e3451a8205d39fa24d0f6b52aa
dudung <dudung@gmail.com>
2019-01-16 06:14:11 +0700

    fin 0013/ssms-the-alt

--- cpp/0013/ssmsTheAlt.cpp
+++ cpp/0013/ssmsTheAlt.cpp
@@ -1,15 +1,17 @@
 /*
-	ssmsThe.cpp
-	Single spring-mass system from theoretical solution
+	ssmsTheAlt.cpp
+	Single spring-mass system from theoretical solution (alternative)
 	
 	Sparisoma Viridi | https://github.com/dudung/butiran
 	
-	Compile: g++ ssmsThe.cpp -o ssmsThe.
-	Execute: ./ssmsThe
+	Compile: g++ ssmsTheAlt.cpp -o ssmsTheAlt
+	Execute: ./ssmsTheAlt
 	
 	20190115
 	0809 Start creating at home.
 	2041 Can find bug
+	20190116
+	0556 Create new version
 */
 
 #include <iostream>
@@ -21,15 +23,14 @@ using namespace std;
 int main(int argc, char *argv[]) {
 	// Verbose usage
 	const char *pname = "ssmsThe";
-	if(argc < 7) {
-		cout << "Usage: " << pname << " [m k xo x0 v0 gamma]";
+	if(argc < 6) {
+		cout << "Usage: " << pname << " [m k xo x0 v0]";
 		cout << endl;
 		cout << "m\tmass" << endl;
 		cout << "k\tspring constant" << endl;
 		cout << "xo\torigin of x" << endl;
 		cout << "x0\tinitial x" << endl;
 		cout << "v0\tinitial v" << endl;
-		cout << "gamma\tdown scale factor of A^2" << endl;
 		return 1;
 	}
 	
@@ -49,28 +50,23 @@ int main(int argc, char *argv[]) {
 	double omega0 = sqrt(k / m);
 	double T0 = 2 * M_PI / omega0;
 	
-	// Set A
-	double gamma = atof(argv[6]); // 0.39489
-	double AA = (x0 - xo)*(x0 - xo) + (v0 / omega0)*(v0 / omega0);
-	double A = sqrt(gamma * AA);
-	
-	// Calculate B
-	double B = sqrt(AA - A * A);
-	
-	// Calculate phi0
-	double AL = omega0 * (x0 - xo) * A;
-	double AR = v0 * B;
-	double phi0 = atan( (AL - AR)/(AL + AR) ) - omega0 * t0;
+	double phi0 = atan( (x0 - xo)/(v0 / omega0) ) - omega0 * t0;
+	double sin0 = sin(omega0 * t0 + phi0);
+	double cos0 = cos(omega0 * t0 + phi0);
+	double C;
+	if(sin0 != 0) {
+		C = (x0 - xo) / sin0;
+	} else {
+		C = (v0 / omega0) / cos0;
+	}
 	
 	cout << "m = " << m << endl;
 	cout << "k = " << k << endl;
 	cout << "omega0 = " << omega0 << endl;
 	cout << "T0 = " << T0 << endl;
-	cout << "gamma = " << gamma << endl;
-	cout << "A = " << A << endl;
-	cout << "B = " << B << endl;
 	cout << "phi0 = " << phi0 << endl;
-		
+	cout << "C = " << C << endl;
+	
 	// Terminate program
 	return 0;
 }
