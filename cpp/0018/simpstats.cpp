beaf7229b81b4e3c46234620dd2f9644f82245ea
dudung <dudung@gmail.com>
2019-01-28 19:55:29 +0700

    beg cpp simpstats

---
+++ cpp/0018/simpstats.cpp
@@ -0,0 +1,21 @@
+/*
+	simpstats.js
+	Some simple statistics function
+	
+	Sparisoma Viridi | https://github.com/dudung/butiran
+	
+	Compile: g++ simpstats.cpp -o simpstats
+	Execute: ./simpstats
+	
+	20190128
+	1953 Derive from simpstats.js at home.	
+*/
+
+#include <iostream>
+#include <sstream>
+#include <vector>
+
+int main(int argc, char *argv[]) {
+	
+	return 0;
+}
