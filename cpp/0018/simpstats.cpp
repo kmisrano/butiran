866b005b48b190a0b4f8f326f6c849a85ba4b1ce
dudung <dudung@gmail.com>
2019-01-28 20:02:30 +0700

    con cpp simpstats

--- cpp/0018/simpstats.cpp
+++ cpp/0018/simpstats.cpp
@@ -14,8 +14,29 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <cstring>
+
+using namespace std;
+
+string strval(vector<double>);
 
 int main(int argc, char *argv[]) {
+	vector<double> x = {2, 3, 5, 2, 9, 7, 2, 8, 2, 1};
+	
+	cout << "x = " << strval(x) << endl;
 	
 	return 0;
+}
+
+// String representation of an array
+string strval(vector<double> x) {
+	ostringstream sout;
+	int N = x.size();
+	for(int i = 0; i < N; i++) {
+		sout << x[i];
+		if(i < N - 1) {
+			sout << ", ";
+		}
+	}
+	return sout.str();
 }
