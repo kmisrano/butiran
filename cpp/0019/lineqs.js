54d40a754f400ab4d3e9e332f4093f561c862613
dudung <dudung@gmail.com>
2019-01-31 04:58:24 +0700

    con 0019 js1 lineqs

--- cpp/0019/lineqs.js
+++ cpp/0019/lineqs.js
@@ -1,193 +1,108 @@
 /*
-	simpstats.js
-	Some simple statistics function
+	lineqs.js
+	Solve system of linear equations
 	
 	Sparisoma Viridi | https://github.com/dudung/butiran
 	
 	Use in HTML file:
-	<script src="simpstats.js"></script>
+	<script src="lineqs.js"></script>
 	
-	20190128
-	1729 Start at home.
-	20190129
-	0404 Fix comment of stdev.
-	
-	Note:
-	Results require browser console with can be opened with
-	buttons combination CTRL+SHIFT+J.
+	20190130
+	1931 Start at home.
 */
 
-// Define global variable
-var ta;
+// Define global variables
+var tin, btn, tout;
 
 // Execute main function
 main();
 
 // Define main function
 function main() {
-	// Create textarea
-	createOutput();
+	// Create layout of visual elements
+	createLayout(tin, btn, tout);
 	
-	// Define data in array form
-	var x = [2, 3, 5, 2, 9, 7, 2, 8, 2, 1];
-	tout("x = " + x.toString().replace(/,/g, ', ') + "\n");
+	// Display initialize values of SLE
+	initSLE(tin);
 	
-	// Get minimum
-	var xmin = min(x);
-	tout("xmin = " + xmin + "\n");
+	// Read array from textarea
+	var M = readArray(tin);
 	
-	// Get maximum
-	var xmax = max(x);
-	tout("xmax = " + xmax + "\n");
-	
-	// Calculate avarage
-	var xavg = avg(x);
-	tout("xavg = " + xavg + "\n");	
-	
-	// Calculate standard deviation
-	var sigma = stdev(x);
-	tout("sigma = " + sigma + "\n");
-	
-	// Sort x
-	sort(x);
-	tout("x (sorted) = " + x.toString().replace(/,/g, ', ')
-		+ "\n");
-	
-	// Get median
-	var xmed = med(x);
-	tout("xmed = " + xmed + "\n");
-	
-	// Get mode
-	var xmod = mod(x);
-	tout("xmod = " + xmod + "\n");
+	// Solve SLE from M
+	var solution = solveSLE(M);
 }
 
-// Get mode of an array
-function mod() {
-	var x = arguments[0];
-	var N = x.length;
-	sort(x);
-	var y = []; // for frequency
-	var z = []; // for value
-	var j;
-	for(var i = 0; i < N; i++) {
-		if(i == 0) {
-			y.push(1);
-			j = 0;
-			z.push(x[i]);
-		} else {
-			if(x[i] == x[i - 1]) {
-				y[j]++;
-			} else {
-				y.push(1);
-				j++;
-				z.push(x[i]);
-			}
+// Solve SLE from an augmented matrix
+function solveSLE() {
+	var M = arguments[0];
+}
+
+// Read array from textarea
+function readArray() {
+	ta = arguments[0];
+	var rows = ta.value.split("\n");
+	var R = rows.length;
+	if(rows[R-1] == 0) {
+		R--;
+	}
+	var mat = [];
+	for(var r = 0; r < R; r++) {
+		var columns = rows[r].split("\t");
+		var C = columns.length;
+		var arow = [];
+		for(var c = 0; c < C; c++) {
+			arow.push(parseFloat(columns[c]));
 		}
+		mat.push(arow);
 	}
-	var imax = 0;
-	for(var i = 1; i < y.length; i++) {
-		if(y[i] > y[imax]) {
-			imax = i;
-		} 
-	}
-	return z[imax];
+	return mat;
 }
 
-// Get median of an array
-function med() {
-	var x = arguments[0];
-	var N = x.length;
-	sort(x);
-	var xmed;
-	if((N / 2) == Math.floor(N / 2)) {
-		var i = N / 2 - 1;
-		xmed = 0.5 * (x[i] + x[i + 1]);
-	} else {
-		var i = (N - 1) / 2;
-		xmed = x[i];
-	}
-	return xmed;
+
+// Display initialize values of SLE
+function initSLE() {
+	var ta = arguments[0];
+	textOut(ta, "1\t1\t1\t1\t1\t15\n");
+	textOut(ta, "2\t-1\t1\t1\t2\t17\n");
+	textOut(ta, "1\t3\t1\t-2\t4\t22\n");
+	textOut(ta, "5\t-4\t-3\t2\t1\t1\n");
+	textOut(ta, "3\t2\t1\t-1\t-2\t-4\n");
 }
 
-// Sort using bubble sort
-function sort() {
-	var x = arguments[0];
-	var N = x.length;
-	for(var i = 0; i < N; i++) {
-		for(var j = i + 1; j < N; j++) {
-			if(x[i] > x[j]) {
-				var xbuf = x[i];
-				x[i] = x[j];
-				x[j] = xbuf;
-			}
-		}
-	}
-}
-
-// Calculate standard deviation of array components
-function stdev() {
-	var x = arguments[0];
-	var N = x.length;
-	var xavg = avg(x);
-	var s2 = 0;
-	for(var i = 0; i < N; i++) {
-		var dx = (x[i] - xavg);
-		s2 += dx*dx;
-	}
-	var xdev = Math.sqrt(s2 / (N - 1));
-	return xdev;
-}
-
-// Calculate average value of array components
-function avg() {
-	var x = arguments[0];
-	var N = x.length;
-	var sx = 0;
-	for(var i = 0; i < N; i++) {
-		sx += x[i];
-	}
-	var xavg = sx / N;
-	return xavg;
-}
-
-// Get maximum value of an array
-function max() {
-	var x = arguments[0];
-	var N = x.length;
-	var xmax = x[0];
-	for(var i = 1; i < N; i++) {
-		if(x[i] > xmax) {
-			xmax = x[i];
-		}
-	}
-	return xmax;
-}
-
-// Get minimum value of an array
-function min() {
-	var x = arguments[0];
-	var N = x.length;
-	var xmin = x[0];
-	for(var i = 1; i < N; i++) {
-		if(x[i] < xmin) {
-			xmin = x[i];
-		}
-	}
-	return xmin;
-}
-
-// Display
-function tout() {
-	ta.value += arguments[0];
+// Add text to textarea
+function textOut() {
+	ta = arguments[0];
+	ta.value += arguments[1];
 	ta.scrollTop = ta.scrollHeight;
 }
 
-// Create a textarea as output
-function createOutput() {
-	ta = document.createElement("textarea");
-	ta.style.width = "350px";
-	ta.style.height = "150px";
-	ta.style.overflowY = "scroll";
-	document.body.append(ta);
+// Create layout of visual elements
+function createLayout() {
+	// Create 1st visual element
+	tin = arguments[0];
+	tin = document.createElement("textarea");
+	tin.style.width = "350px";
+	tin.style.height = "150px";
+	tin.style.overflowY = "scroll";
+	tin.style.float = "left";
+	
+	// Create 2nd visual element
+	btn = arguments[1];
+	btn = document.createElement("button");
+	btn.innerHTML = "Solve";
+	btn.style.float = "left";
+	
+	// Create 3rd visual element
+	tout = arguments[2];
+	tout = document.createElement("textarea");
+	tout.style.width = "200px";
+	tout.style.height = "150px";
+	tout.style.overflowY = "scroll";
+	tout.style.float = "left";
+	
+	// Arrange elements
+	document.body.append(tin);
+	document.body.append(btn);
+	document.body.append(tout);
 }
+
