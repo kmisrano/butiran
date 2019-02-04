9849aef8caa4d41fe262051a24ccf0b74f34d6a9
dudung <dudung@gmail.com>
2019-02-04 20:17:10 +0700

    beg 0104

--- cpp/0104/randgen.js
+++ cpp/0104/randgen.js
@@ -7,8 +7,10 @@
 	Include: <script src="randgen.js"></script> in a HTML file
 	Execute: Refresh web browser viewing the HTML file
 	
-	20190204
-	1928 Beg zuhause.
+	20190204 Zuhause 1928 beg, 2016 end.
+	
+	References
+	20190204 https://stackoverflow.com/a/33898676/9475509
 */
 
 // Execute main function
@@ -16,20 +18,61 @@ main();
 
 // Define main function
 function main() {
-	var str = "";
+	// Create textarea as output
+	var ta = document.createElement("textarea");
+	ta.style.overflowY = "scroll";
+	ta.style.width = "200px";
+	ta.style.height = "200px";
+	document.body.append(ta);
+	
+	// Define arrays for number and its statistic frequency
 	var min = 3;
 	var max = 7;
+	var num = range(min, max);
+	var sum = [...num];
+	sum.fill(0);
+	
+	// Generate random number N times
 	var N = 100;
 	for(var i = 0; i < N; i++) {
-		var num = randInt(min, max);
-		str += num + " ";
+		var x = randInt(min, max);
+		var idx = num.indexOf(x);
+		sum[idx]++;
 	}
-	console.log(str);
+	
+	// Display results in textarea
+	var digit = Math.floor(Math.log10(N));
+	var str = "N = " + N + "\n";
+	str += "min = " + min + "\n";
+	str += "max = " + max + "\n";
+	str += "\n";
+	str += "Distribution\n";
+	for(var i = 0; i < num.length; i++) {
+		var x = (sum[i] / N).toFixed(digit);
+		str += num[i] + "\t" + sum[i] + "\t" + x + "\n";
+	}
+	ta.value = str;
+	
+	// Return value, even it is not necessary
+	return 0;
 } 
 
-// Define random number between two integers (inclusive)
-function randInt(lo, hi) {
-	var range = hi - lo;
-	var randNum = lo + Math.floor((range + 1) * Math.random());
-	return randNum;
+// Generate a random number between two integers (inclusive)
+function randInt() {
+	var min = arguments[0];
+	var max = arguments[1];
+	var range = max - min;
+	var num = min + Math.floor((range + 1) * Math.random());
+	return num;
+}
+
+// Create array of integer from min to max
+function range() {
+	var beg = arguments[0];
+	var end = arguments[1];
+	var num = [];
+	for(var i = beg; i <= end; i++) {
+		num.push(i);
+	}
+	return num;
 }
