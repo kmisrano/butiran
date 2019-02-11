751e4dba9c18ec92d514bb672d44416092985737
dudung <dudung@gmail.com>
2019-02-11 18:11:45 +0700

    cont 0020

--- cpp/0020/mat.cpp
+++ cpp/0020/mat.cpp
@@ -9,6 +9,14 @@
 	
 	20190211
 	Create this program waehrend zuhause krank.
+	
+	References
+	1 Iterate through a C++ Vector
+	url https://stackoverflow.com/a/12702744/9475509
+	2 Initialize a two dimensional C++ Vector
+	url https://stackoverflow.com/q/17663186/9475509
+	3 Iterate through a two dimensional C++ Vector
+	url https://stackoverflow.com/a/23303597/9475509
 */
 
 #include <iostream>
@@ -18,26 +26,45 @@
 
 using namespace std;
 
+void disp0(vector<vector<int>>);
+void disp1(vector<vector<int>>);
+
 int main(int argc, char *argv[]) {
 	
-	vector<vector<int>> M = {
+	vector<vector<int>> N = {
 		{1, 2},
 		{3, 4, 8, 9},
 		{5, 6},
 	};
 	
+	disp0(N);
+	disp1(N);
+	
+	return 0;
+}
+
+// Display matrix of C++ Vector without iterator
+void disp0(vector<vector<int>> M) {
 	for(int i = 0; i < M.size(); i++) {
 		for(int j = 0; j < M[i].size(); j++) {
-			cout << M[i][j] << " ";
+			cout << M[i][j];
+			if(j < M[i].size() - 1) {
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}	
+}
+
+// Display matrix of C++ Vector using iterator
+void disp1(vector<vector<int>> M) {
+	for(vector<int> rows: M) {
+		for(int val: rows) {
+			cout << val;
+			if(val < rows.end) {
+				cout << " ";
+			}
 		}
 		cout << endl;
 	}
-	
-	
-	vector<int> J = {9, 8, 7};
-	for(int i: J) {
-		cout << i << endl;
-	}
-	
-	return 0;
 }
